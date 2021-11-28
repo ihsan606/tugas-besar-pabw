ef7a53fedcbe1b9f1b051fa7f4a2aee5264acb3b
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-11-28 20:25:30 +0700

    test

--- app/core/App.php
+++ app/core/App.php
@@ -2,8 +2,18 @@
 
 class App 
 {
-  public function __construct(){
-    echo "test";
+  public function __construct()
+  {
+    $url = $this->parseURL();
+    var_dump($url);
+  }
+
+  public function parseURL()
+  {
+    if(isset($_GET['url'])){
+      $url = $_GET['url'];
+      return $url;
+    }
   }
     
 }
