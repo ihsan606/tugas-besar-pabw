60e08852fe105d81814ca3ebcc39608b7b981726
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-01 17:48:09 +0700

    test

--- app/init.php
+++ app/init.php
@@ -4,6 +4,7 @@
 //   require_once 'core/' . $class . '.php';
 // });
 
+require_once '../vendor/autoload.php';
 require_once 'core/App.php';
 require_once 'core/Controller.php';
 require_once 'config/config.php';
