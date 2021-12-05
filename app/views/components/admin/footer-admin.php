e449cca5b0153260a9a4cb670c5b4ff2dc285b2a
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-06 01:31:31 +0700

    test

--- app/views/components/admin/footer-admin.php
+++ app/views/components/admin/footer-admin.php
@@ -61,7 +61,7 @@
   <script src="<?=BASEURL;?>/assets/js/core/popper.min.js"></script>
   <script src="<?=BASEURL;?>/assets/js/core/bootstrap.min.js"></script>
   <script src="<?=BASEURL;?>/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-  <!--  Google Maps Plugin    -->
+  <!-- Google Maps Plugin -->
   <!-- Place this tag in your head or just before your close body tag. -->
   <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
   <!-- Chart JS -->
