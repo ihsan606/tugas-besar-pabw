970d719aaf95b703315e3e94e78696cb801c6b00
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-08 10:56:14 +0700

    test

--- app/views/pages/admin/kelola-kategori.php
+++ app/views/pages/admin/kelola-kategori.php
@@ -1,8 +1,28 @@
 <div class="card card-tasks" style = 'height : 100%;'>
   <div class="card-header ">
-    <h3>Kategori</h3>
+    <h3 style = "margin : 0px 0px 0px 0px;">Kelola Kategori</h3>
   </div>
-  <div class="card-body ">
+  <div class="card-body">
+   
+    <div class="form-group-apend">
+      <div class="row pb-3">
+        <div class="col-md-auto align-self-center">
+          <div class="input-group-apend">
+            <button class="btn btn-sm p-2"><i class="fa fa-plus-circle mr-2"></i>TAMBAH KATEGORI</button>
+          </div>
+        </div>
+        <div class="col align-self-center px-0">
+          <div class="input-group-prepended ">
+            <input type="text" class="form-control" placeholder="cari berdasarkan nama kategori">        
+          </div>
+        </div>
+        <div class="col-md-auto align-self-center">
+          <div class="input-group-apend">
+            <button class="btn btn-sm p-2"><i class="fa fa-search mr-2"></i>SEARCH</button>
+          </div>
+        </div>
+      </div>
+    </div>
     <table class="table">
       <thead>
         <tr>
@@ -25,20 +45,20 @@
         <tbody>
           <!-- buat perulangan untuk menampilkan kategori -->
           <?php 
-          for($i = 0; $i < 30; $i++){
+          for($i = 1; $i < 31; $i++){
             echo "
               <tr>
                 <td>
                   <p>$i</p>
                 </td>
                 <td>
-                  <p class='title'>Update the Documentation</p>
+                  <p class='title'>Kategori ke-$i</p>
                 </td>
                 <td class='td-actions text-center'>
-                  <button type='button' rel='tooltip' title='' class='btn btn-link' data-original-title='Edit Kategori'>
+                  <button type='button' rel='tooltip' title='' class='btn btn-link px-1' data-original-title='Edit Kategori'>
                     <a class='text-light' href='#' style = 'font-size : 16px;'><i class='bi-pencil' role='img' aria-label='Pencil'></i></a>
                   </button>
-                  <button type='button' rel='tooltip' title='' class='btn btn-link' data-original-title='Hapus Kategori'>
+                  <button type='button' rel='tooltip' title='' class='btn btn-link px-1' data-original-title='Hapus Kategori'>
                     <a class='text-light' href='#' style = 'font-size : 16px;'><i class='bi-trash' role='img' aria-label='Trash'></i></a>
                   </button>
                 </td>
--- app/views/pages/admin/kelola-menu.php
+++ app/views/pages/admin/kelola-menu.php
@@ -1,1 +1,81 @@
-<h1>Halaman Kelola Menu</h1>
+<div class="card card-tasks" style = 'height : 100%;'>
+  <div class="card-header ">
+    <h3 style = "margin : 0px 0px 0px 0px;">Kelola Menu</h3>
+  </div>
+  <div class="card-body">
+   
+    <div class="form-group-apend">
+      <div class="row pb-3">
+        <div class="col-md-auto align-self-center">
+          <div class="input-group-apend">
+            <button class="btn btn-sm p-2"><i class="fa fa-plus-circle mr-2"></i>TAMBAH MENU</button>
+          </div>
+        </div>
+        <div class="col align-self-center px-0">
+          <div class="input-group-prepended ">
+            <input type="text" class="form-control" placeholder="cari berdasarkan nama menu">        
+          </div>
+        </div>
+        <div class="col-md-auto align-self-center">
+          <div class="input-group-apend">
+            <button class="btn btn-sm p-2"><i class="fa fa-search mr-2"></i>SEARCH</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th class="text-left" width = "20px"><p class="title">No</p></th>
+          <th class="text-left"><p class="title">Nama Menu</p></th>
+          <th class="text-left" width = "240px"><p class="title">Gambar</p></th>
+          <th class="text-center" width = "120px"><p class="title">Action</p></th>
+        </tr>
+      </thead>
+    </table>
+    <div class="table-full-width table-responsive">
+      <table class="table">
+        <thead>
+          <tr>
+            <th class="text-left" width = "20px"></p></th>
+            <th class="text-left"></p></th>
+            <th class="text-left" width = "240px"></p></th>
+            <th class="text-center" width = "120px"></p></th>
+            </tr>
+          <tr>
+        </thead>
+        <tbody>
+          <!-- buat perulangan untuk menampilkan kategori -->
+          <?php 
+          for($i = 1; $i < 31; $i++){
+            echo "
+              <tr>
+                <td>
+                  <p>$i</p>
+                </td>
+                <td>
+                  <p class='title'>Menu ke-$i</p>
+                </td>
+                <td>
+                  <img src='' alt='' height = '40px' width = '40px'>
+                </td>
+                <td class='td-actions text-center'>
+                  <button type='button' rel='tooltip' title='' class='btn btn-link px-1' data-original-title='Hapus Kategori'>
+                    <a class='text-light' href='#' style = 'font-size : 16px;'><i class='bi-bag-check' role='img' aria-label='Trash'></i></a>
+                  </button>
+                  <button type='button' rel='tooltip' title='' class='btn btn-link px-1' data-original-title='Edit Kategori'>
+                    <a class='text-light' href='#' style = 'font-size : 16px;'><i class='bi-pencil' role='img' aria-label='Pencil'></i></a>
+                  </button>
+                  <button type='button' rel='tooltip' title='' class='btn btn-link px-1' data-original-title='Hapus Kategori'>
+                    <a class='text-light' href='#' style = 'font-size : 16px;'><i class='bi-trash' role='img' aria-label='Trash'></i></a>
+                  </button>
+                </td>
+              </tr>
+            ";
+          }
+          ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>   
