f3fea713785932f8f8c618ef81b7e92d8ea58c94
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-09 01:45:02 +0700

    test

--- app/controllers/admin/Tambah_Kategori_Controller.php
+++ app/controllers/admin/Tambah_Kategori_Controller.php
@@ -12,22 +12,28 @@ class Tambah_Kategori_Controller extends Controller{
   }
 
   public function store(){
-    // if(issset($_POST['upload'])){
-      $name = $_POST['nama']; 
-      $image_name = $_FILES['gambar']['name'];
-      $direktori = 'img/categories/';
-      move_uploaded_file($_FILES['gambar']['tmp_name'], $direktori.$image_name);
-      // var_dump($_FILES);
+    $name = $_POST['nama']; 
 
-      $category = Category::create([
-        'name' => $name,
-        'image' => $image_name,
-        'slug' => Str::slug($name, '-')
-      ]);
-
-      if($category){
-        header('Location: ' . BASEURL . '/admin/Kelola_Kategori');
+    $image_name = $_FILES['gambar']['name'];
+    $allowed_extension = ['png', 'jpg', 'jpeg'];
+    $x = explode('.', $image_name);
+    $extension = strtolower(end($x));
+    $size = $_FILES['gambar']['size'];
+    if(in_array($extension, $allowed_extension) === true){
+      echo 'type true';
+      if($size < 2000000){
+        echo 'ukuran true';
+        // $direktori = 'img/categories/';
+        // move_uploaded_file($_FILES['gambar']['tmp_name'], $direktori.$image_name);
+        // $category = Category::create([
+        //   'name' => $name,
+        //   'image' => $image_name,
+        //   'slug' => Str::slug($name, '-')
+        // ]);
+        // if($category){
+        //   header('Location: ' . BASEURL . '/admin/Kelola_Kategori');
+        // }
       }
-    // }   
+    }  
   }
 }
