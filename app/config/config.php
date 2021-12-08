06519c012e72ba43dbe3b97acbbe063baf10b661
ihsan606 <ihsansyafiul@gmail.com>
2021-12-08 15:49:41 +0700

    commit

--- app/config/config.php
+++ app/config/config.php
@@ -1,3 +1,3 @@
 <?php 
 
-define('BASEURL', 'http://localhost:8080/tugas-besar-pabw/public');
+define('BASEURL', 'http://rezerva.test:4040/tugas-besar-pabw/public');
--- app/models/Order.php
+++ app/models/Order.php
@@ -1,7 +1,26 @@
-<?php 
+<?php
+
 namespace App\models;
+
 use Illuminate\Database\Eloquent\Model;
 
-class Order extends Model{
-  protected $guarded = ['id'];
+class Order extends Model
+{
+    protected $guarded = ['id'];
+
+
+    public function menu()
+    {
+        return $this->belongsTo(Menu::class);
+    }
+
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class);
+    }
+
+    public function table()
+    {
+        return $this->belongsTo(Table::class);
+    }
 }
