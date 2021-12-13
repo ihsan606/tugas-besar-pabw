6ad79afa88503e2b9f6b3b1fcf25e07d1c8d4066
20523034@students.uii.ac.id <you@example.com>
2021-12-13 21:18:53 +0700

    edit daftar pesanan

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php 
 
-define('BASEURL', 'http://localhost:8080/tugas-besar-pabw/public');
+define('BASEURL', 'http://localhost/tugas-besar-pabw/public');
 // define('BASEURL', 'http://rezerva.test:4040//tugas-besar-pabw/public');
 
-define('APPURL', 'http://localhost:8080/tugas-besar-pabw/app');
+define('APPURL', 'http://localhost/tugas-besar-pabw/app');
 // 'http://localhost:8080/tugas-besar-pabw/public'
