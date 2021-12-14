9d8f2556d9d20745a62fb0012001c57933a23170
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-14 23:45:50 +0700

    test

--- app/controllers/customer/Home_Controller.php
+++ app/controllers/customer/Home_Controller.php
@@ -1,9 +1,19 @@
 <?php 
 
+require '../vendor/autoload.php';
+use App\models\Menu;
+use App\models\Category;
+
 class Home_Controller extends Controller{
   public function index()
-  { 
-    $data = ['title' => 'Home',];
+  {
+    $menu_andalan_id = Category::where('name', 'Menu Andalan')->first()->id;
+    $menu_terbaru_id = Category::where('name', 'Menu Terbaru')->first()->id;
+    $data = [
+      'title' => 'Home',
+      'menu_andalan' => Menu::with('category')->where('category_id', $menu_andalan_id)->get(),
+      'menu_terbaru' => Menu::with('category')->where('category_id', $menu_terbaru_id)->get(),
+    ];
     $this->view('home', $data, 'customer');
   }
 }
