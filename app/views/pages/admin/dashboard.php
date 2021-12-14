91415d5bcbbd779cda1a1c4786db5b7a41d257ea
20523034@students.uii.ac.id <you@example.com>
2021-12-15 00:18:03 +0700

    update dashboard

--- app/views/pages/admin/dashboard.php
+++ app/views/pages/admin/dashboard.php
@@ -66,7 +66,133 @@
         <div class="chart">
           <div id="chart" class="card"></div>
         </div>
+        <div class="card">
+    <table class="table">
+        <thead class="thead-light">
+            <tr>
+                <th class="text-center">No</th>
+                <th>Name</th>
+                <th>No meja</th>
+                <th class="text-left">Daftar Menu</th>
+                <th class="text-right">Actions</th>
+            </tr>
+        </thead>
+        <tbody class="table-striped">
+            <tr>
 
+                <td class="text-center">1</td>
+                <td class="td-top">Devano</td>
+                <td>1</td>
+                <td class="text-right">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th class="text-left">no</th>
+                                <th class="text-left">nama makanan</th>
+                                <th class="text-left">jumlah</th>
+                                <th class="text-center">Keterangan</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="text-left">1</td>
+                                <td class="text-left">Seafood Udang </td>
+                                <td class="text-left">4</td>
+                                <td class="text-center">Tidak pake nasi</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+                <td class="td-actions text-right">
+                    <button type="button" rel="tooltip" class="btn btn-info btn-sm btn-icon">
+                        <i class="tim-icons icon-single-02"></i>
+                    </button>
+                    <button type="button" rel="tooltip" class="btn btn-success btn-sm btn-icon">
+                        <i class="tim-icons icon-settings"></i>
+                    </button>
+                    <button type="button" rel="tooltip" class="btn btn-danger btn-sm btn-icon">
+                        <i class="tim-icons icon-simple-remove"></i>
+                    </button>
+                </td>
+            </tr>
+            <tr>
+
+                <td class="text-center">1</td>
+                <td class="td-top">Devano</td>
+                <td>1</td>
+                <td class="text-right">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th class="text-left">no</th>
+                                <th class="text-left">nama makanan</th>
+                                <th class="text-left">jumlah</th>
+                                <th class="text-center">Keterangan</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="text-left">1</td>
+                                <td class="text-left">Seafood Udang </td>
+                                <td class="text-left">4</td>
+                                <td class="text-center">Tidak pake nasi</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+                <td class="td-actions text-right">
+                    <button type="button" rel="tooltip" class="btn btn-info btn-sm btn-icon">
+                        <i class="tim-icons icon-single-02"></i>
+                    </button>
+                    <button type="button" rel="tooltip" class="btn btn-success btn-sm btn-icon">
+                        <i class="tim-icons icon-settings"></i>
+                    </button>
+                    <button type="button" rel="tooltip" class="btn btn-danger btn-sm btn-icon">
+                        <i class="tim-icons icon-simple-remove"></i>
+                    </button>
+                </td>
+            </tr>
+            <tr>
+
+                <td class="text-center">1</td>
+                <td class="td-top">Devano</td>
+                <td>1</td>
+                <td class="text-right">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th class="text-left">no</th>
+                                <th class="text-left">nama makanan</th>
+                                <th class="text-left">jumlah</th>
+                                <th class="text-center">Keterangan</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="text-left">1</td>
+                                <td class="text-left">Seafood Udang </td>
+                                <td class="text-left">4</td>
+                                <td class="text-center">Tidak pake nasi</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+                <td class="td-actions text-right">
+                    <button type="button" rel="tooltip" class="btn btn-info btn-sm btn-icon">
+                        <i class="tim-icons icon-single-02"></i>
+                    </button>
+                    <button type="button" rel="tooltip" class="btn btn-success btn-sm btn-icon">
+                        <i class="tim-icons icon-settings"></i>
+                    </button>
+                    <button type="button" rel="tooltip" class="btn btn-danger btn-sm btn-icon">
+                        <i class="tim-icons icon-simple-remove"></i>
+                    </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+</div>
       </div>
       <div class="col-md-4">
         <h2>Income</h2>
@@ -85,12 +211,10 @@
           </div>
 
         </div>
+        
       </div>
     </div>
   </div>
 </div>
 
 
-<?php
-  require 
-?>
