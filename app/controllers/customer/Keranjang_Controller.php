b23dbec9154f36ed125c7fa1316018b9f6640e4d
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-15 02:55:49 +0700

    test

--- app/controllers/customer/Keranjang_Controller.php
+++ app/controllers/customer/Keranjang_Controller.php
@@ -6,15 +6,10 @@ use App\models\Menu;
 class Keranjang_Controller extends Controller{
   public function index($snap_token = '')
   {
-    if($_SESSION['keranjang']['menus'] === NULL){
-      if($snap_token != ''){
-      
-      }
-      $data = ['title' => 'Keranjang',];
-      $this->view('keranjang', $data, 'customer');
-    }else{
-      $data = ['title' => 'Keranjang Kosong',];
-      $this->view('keranjang-kosong', $data, 'customer');
+    if($snap_token != ''){
+    
     }
+    $data = ['title' => 'Keranjang',];
+    $this->view('keranjang', $data, 'customer');
   }
 }
