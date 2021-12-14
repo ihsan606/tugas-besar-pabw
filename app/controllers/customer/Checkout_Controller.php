cf5208bad0d356478198ee3491f683e55bb395a9
ihsan606 <ihsansyafiul@gmail.com>
2021-12-14 22:32:10 +0700

    badge

--- app/controllers/customer/Checkout_Controller.php
+++ app/controllers/customer/Checkout_Controller.php
@@ -1,14 +1,18 @@
 <?php
 
+use App\models\Customer;
 use App\models\Invoice;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
+use Midtrans\Snap;
 
 require '../vendor/autoload.php';
 
-class Checkout_Controller extends Controller {
+class Checkout_Controller extends Controller
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         // Set your Merchant Server Key
         \Midtrans\Config::$serverKey = 'SB-Mid-server-J8w5w1HZkc3QYjMNVpq-C1lf';
         // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
@@ -20,12 +24,19 @@ class Checkout_Controller extends Controller {
     }
 
 
-    public function store($data) {
-
-        DB::transaction(function () use ($data) {
-
+    public function store()
+    {
+        session_start();
+        
+       
             //create customer table
-            //create invoice number
+            $customer = Customer::create([
+                'name' => $_POST['customer_name'],
+                'email' => 'admin@gmail.com',
+                'password' => 'admin',
+            ]);
+
+            // // create invoice number
             $length = 10;
             $random = '';
 
@@ -33,13 +44,61 @@ class Checkout_Controller extends Controller {
                 $random .= rand(0, 1) ? rand(0, 9) : chr(rand(ord('a'), ord('z')));
             }
 
-            //generate no invoice
-            $no_invoice = 'INV-'.Str::upper($random);
+            // //generate no invoice
+            $no_invoice = 'INV-' . Str::upper($random);
             $invoice = Invoice::create([
-                
+                'invoices' => $no_invoice,
+                'customer_id' => $customer->id,
+                'grand_total' => $_POST['grand_total'],
+                'status' =>'pending',
             ]);
-            
-        });
-    }
+            $menus = $_SESSION['keranjang']['menus'];
+            for ($i=0; $i < count($menus); $i++) { 
+                $menu = $menus[$i];
+                $invoice->orders()->create([
+                    'invoice_id' => $invoice->id,
+                    'menu_id' => $menu['menu']['0']->id,
+                    'qty' => $menu['jumlah'],
+                    'table_id'=>$_POST['no_meja'],
+                    'description' =>$menu['keterangan'],
+                    'status' =>'dikonfirmasi',
+                    'price'=>$menu['menu'][0]->price*(100-$menu['menu'][0]->discount)/100
+                ]);
+            }
 
+            //hapus keranjang
+            session_unset();
+            //create transaction to midtrans, then save snap token
+            $payload = [
+                'transaction_details' => [
+                    'order_id'      => $invoice->invoice,
+                    'gross_amount'  => $invoice->grand_total,
+                ],
+                'customer_details' => [
+                    'first_name'       => Customer::whereId($customer->id)->first()->name,
+                    'email'            => Customer::whereId($customer->id)->first()->email,
+                    'phone'            => '081983744023',
+                ]
+            ];
+
+            $snap_token = Snap::getSnapToken($payload);
+
+            //update snap_token
+            $invoice->snap_token = $snap_token;
+            $invoice->save();
+            header('location:'. BASEURL. 'customer/keranjang/');
+
+
+            
+
+
+
+
+            //store order by invoice
+       
+        
+
+
+
+    }
 }
