519f6094d907c52bd1e1de95343caca2414ddae1
ihsan606 <ihsansyafiul@gmail.com>
2021-12-14 20:46:08 +0700

    commit

--- app/views/pages/customer/home.php
+++ app/views/pages/customer/home.php
@@ -1,4 +1,27 @@
+<<<<<<< HEAD
 <h1>Halaman Home</h1>
+=======
+<?php 
+session_start();
+$_SESSION['keranjang'] = [
+  
+    'customer_name'=>'ihsan',
+    'id_meja' =>1,
+    'menus' =>[
+        [
+            'id'=>7,
+            'qty'=>2,
+            'price'=>0,
+        ],
+        [
+            'id'=>10,
+            'qty'=>2,
+            'price'=>0,
+        ]
+    ]
+];
+
+>>>>>>> 9439379 (commit)
 
 
 
