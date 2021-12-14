6c0228cf0572c2d2e9ec62793257e2ef1fce805a
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-14 20:57:06 +0700

    test

--- app/controllers/customer/Detail_Menu_Controller.php
+++ app/controllers/customer/Detail_Menu_Controller.php
@@ -12,4 +12,38 @@ class Detail_Menu_Controller extends Controller{
     ];
     $this->view('detail-menu', $data, 'customer');
   }
+
+  public function store($menu_id){
+    $jumlah = $_POST['jumlah'];
+    $keterangan = $_POST['keterangan'];
+
+    session_start();
+    if(isset($_SESSION['keranjang']['menus']) == 0){
+      $_SESSION['keranjang']['menus'] = []; 
+    }
+
+    $cek = false;
+    for($i = 0; $i < count($_SESSION['keranjang']['menus']); $i++){
+      if($_SESSION['keranjang']['menus'][$i]['menu_id'] == $menu_id){
+        $cek = true;
+        $_SESSION['keranjang']['menus'][$i]['jumlah'] = $_SESSION['keranjang']['menus'][$i]['jumlah'] + $jumlah;
+        $_SESSION['keranjang']['menus'][$i]['keterangan'] = $_SESSION['keranjang']['menus'][$i]['keterangan'] . ", " . $keterangan;
+        break;
+      }
+    }
+
+    if(!$cek){
+      array_push(
+        $_SESSION['keranjang']['menus'],
+        [
+          'menu_id' => $menu_id,
+          'menu' => Menu::where('id', $menu_id)->get(),
+          'jumlah' => $jumlah,
+          'keterangan' => $keterangan,
+        ]
+      );
+    }
+    
+    header('Location: ' . BASEURL . '/customer/keranjang');
+  }
 }
