e05cd201e25101c41d349db60ce72030744017a6
20523034@students.uii.ac.id <you@example.com>
2021-12-14 21:51:50 +0700

    coba update

--- app/views/pages/admin/dashboard.php
+++ app/views/pages/admin/dashboard.php
@@ -1,22 +1,29 @@
-<h1>Halaman Dashboard</h1>
+<div class="content card p-3">
+  <div class="container">
 
 
+  </div>
 
-<div class="content">
-  <div class="container-fill">
+
+  <div class="container-fill h-100 ">
     <div class="row">
-      <div class="col-md-12">
-        <div id="chart" class="card"></div>
+      <div class="col-md-8 ">
+        <div class="head-left">
+          <div class="nama admin">
+            <h2>Hai <?="nama admin"?></h2>
+          </div>
+          <div class="search">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          </div>
+        </div>
+        <div class="chart">
+          <div id="chart" class="card"></div>
+        </div>
+        
+      </div>
+      <div class="col-md-4">
+        <h2>Income</h2>
       </div>
     </div>
-    <div class="row">
-      <div class="col-md-6 card">
-
-      </div>
-      <div class="col-md-6 card">
-
-      </div>
-    </div>
-
   </div>
 </div>
