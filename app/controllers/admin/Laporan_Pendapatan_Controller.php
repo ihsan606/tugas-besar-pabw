669cea1fc738413127c4aab443870b145c448662
ihsan606 <ihsansyafiul@gmail.com>
2021-12-17 01:35:35 +0700

    commit

--- app/controllers/admin/Laporan_Pendapatan_Controller.php
+++ app/controllers/admin/Laporan_Pendapatan_Controller.php
@@ -2,13 +2,14 @@
 
 require '../vendor/autoload.php';
 use App\models\Order;
+use Illuminate\Support\Carbon;
 
 class Laporan_Pendapatan_Controller extends Controller{
   public function index()
   {
     $data = [
       'title' => 'Laporan Pendapatan',
-      'pendapatan_hari_ini' => Order::where('satus', 'diantar')->where('created_at', '>=', Carbon::now())
+      'pendapatan_hari_ini' => Order::where('satus', 'diantar')->where('created_at', '>=', Carbon)
     ];
     $this->view('laporan-pendapatan', $data, 'admin');
   }
