d944e03ae40c7ce27dacd9fb10e7ef5b70c73df5
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-17 01:33:59 +0700

    test

--- app/controllers/admin/Laporan_Pendapatan_Controller.php
+++ app/controllers/admin/Laporan_Pendapatan_Controller.php
@@ -1,9 +1,15 @@
 <?php 
 
+require '../vendor/autoload.php';
+use App\models\Order;
+
 class Laporan_Pendapatan_Controller extends Controller{
   public function index()
   {
-    $data = ['title' => 'Laporan Pendapatan',];
+    $data = [
+      'title' => 'Laporan Pendapatan',
+      'pendapatan_hari_ini' => Order::where('satus', 'diantar')->where('created_at', '>=', Carbon::now())
+    ];
     $this->view('laporan-pendapatan', $data, 'admin');
   }
 }
--- app/views/pages/admin/laporan-pendapatan.php
+++ app/views/pages/admin/laporan-pendapatan.php
@@ -1,1 +1,8 @@
-<div class="row"></div>
+<div class="row">
+  <div class="col card mr-2" style='height: 100px;'>
+
+  </div>
+  <div class="col card ml-2" style='height: 100px;'>
+
+  </div>
+</div>
