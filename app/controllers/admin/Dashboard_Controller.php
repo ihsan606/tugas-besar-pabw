d4a83edd32620964a8ad10eaed94ad70dac8e506
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-17 06:31:27 +0700

    test

--- app/controllers/admin/Dashboard_Controller.php
+++ app/controllers/admin/Dashboard_Controller.php
@@ -7,12 +7,18 @@ use Illuminate\Support\Carbon;
 class Dashboard_Controller extends Controller{
   public function index()
   { 
+    $data_perbulan = [];
+    for($i = 1; $i < 13; $i++){
+      array_push($data_perbulan, Invoice::where('status', 'success')->whereMonth('created_at', $i)->sum('grand_total'));
+    }
+
     $data = [
       'title' => 'Dashboard',
       'pendapatan_hari_ini' => money_format(Invoice::where('status', 'success')->where('created_at', '>=', Carbon::today())->sum('grand_total')),
       'pendapatan_minggu_ini' => money_format(Invoice::where('status', 'success')->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->sum('grand_total')),
       'pendapatan_bulan_ini' => money_format(Invoice::where('status', 'success')->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->sum('grand_total')),
       'pendapatan_tahun_ini' => money_format(Invoice::where('status', 'success')->whereBetween('created_at', [Carbon::now()->startOfYear(), Carbon::now()->endOfYear()])->sum('grand_total')),
+      'pendapatan_perbulan' => $data_perbulan,
     ];
     $this->view('dashboard', $data, 'admin');
   }
--- app/controllers/admin/Laporan_Pendapatan_Controller.php
+++ app/controllers/admin/Laporan_Pendapatan_Controller.php
@@ -13,12 +13,14 @@ class Laporan_Pendapatan_Controller extends Controller{
     for($i = 1; $i < 13; $i++){
       array_push($data_perbulan, Invoice::where('status', 'success')->whereMonth('created_at', $i)->sum('grand_total'));
     }
+
     $data = [
       'title' => 'Laporan Pendapatan',
       'pendapatan_hari_ini' => money_format(Invoice::where('status', 'success')->where('created_at', '>=', Carbon::today())->sum('grand_total')),
       'pendapatan_minggu_ini' => money_format(Invoice::where('status', 'success')->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->sum('grand_total')),
       'pendapatan_bulan_ini' => money_format(Invoice::where('status', 'success')->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->sum('grand_total')),
       'pendapatan_tahun_ini' => money_format(Invoice::where('status', 'success')->whereBetween('created_at', [Carbon::now()->startOfYear(), Carbon::now()->endOfYear()])->sum('grand_total')),
+      'pendapatan_perbulan' => $data_perbulan,
     ];
     $this->view('laporan-pendapatan', $data, 'admin');
   }
