530920b1e5e58900ee0152ab70f5d7ed616ae4fe
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-17 02:50:31 +0700

    test

--- app/controllers/admin/Laporan_Pendapatan_Controller.php
+++ app/controllers/admin/Laporan_Pendapatan_Controller.php
@@ -1,7 +1,7 @@
 <?php 
 
 require '../vendor/autoload.php';
-use App\models\Order;
+use App\models\Invoice;
 use Illuminate\Support\Carbon;
 
 class Laporan_Pendapatan_Controller extends Controller{
@@ -9,7 +9,10 @@ class Laporan_Pendapatan_Controller extends Controller{
   {
     $data = [
       'title' => 'Laporan Pendapatan',
-      'pendapatan_hari_ini' => Order::where('satus', 'diantar')->where('created_at', '>=', Carbon)
+      'pendapatan_hari_ini' => Invoice::where('status', 'success')->where('created_at', '>=', Carbon::today())->sum('grand_total'),
+      'pendapatan_minggu_ini' => Invoice::where('status', 'success')->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->sum('grand_total'),
+      'pendapatan_bulan_ini' => Invoice::where('status', 'success')->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->sum('grand_total'),
+      'pendapatan_tahun_ini' => Invoice::where('status', 'success')->whereBetween('created_at', [Carbon::now()->startOfYear(), Carbon::now()->endOfYear()])->sum('grand_total'),
     ];
     $this->view('laporan-pendapatan', $data, 'admin');
   }
--- app/views/pages/admin/laporan-pendapatan.php
+++ app/views/pages/admin/laporan-pendapatan.php
@@ -1,6 +1,20 @@
 <div class="row">
   <div class="col card mr-2" style='height: 100px;'>
-
+    <?php 
+    $pendapatan_hari_ini = money_format($data['pendapatan_hari_ini']);
+    $pendapatan_minggu_ini = money_format($data['pendapatan_minggu_ini']);
+    $pendapatan_bulan_ini = money_format($data['pendapatan_bulan_ini']);
+    $pendapatan_tahun_ini = money_format($data['pendapatan_tahun_ini']);
+    
+      echo"
+      <h1>$pendapatan_hari_ini</h1>
+      <h1>$pendapatan_minggu_ini</h1>
+      <h1>$pendapatan_bulan_ini</h1>
+      <h1>$pendapatan_tahun_ini</h1>
+      ";
+  
+    ?>
+    <h1>zcbxcvbcxngn</h1>
   </div>
   <div class="col card ml-2" style='height: 100px;'>
 
