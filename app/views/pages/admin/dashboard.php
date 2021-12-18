5bd0cc923b05e392f77210a7e748ff50855cf918
20523034@students.uii.ac.id <you@example.com>
2021-12-19 00:16:31 +0700

    update chart

--- app/views/pages/admin/dashboard.php
+++ app/views/pages/admin/dashboard.php
@@ -1,10 +1,9 @@
-
 <script src="<?= BASEURL; ?>/assets/demo/demo.js"></script>
-  <div class="container-fill">
-    <!-- Laporan Pendapatan -->
-    <div class="row">
-      <div class="col-md-8 pb-0 mb-0 d-flex">
-        <!-- <div class="head-left">
+<div class="container-fill">
+  <!-- Laporan Pendapatan -->
+  <div class="row">
+    <div class="col-md-8 pb-0 mb-0 d-flex">
+      <!-- <div class="head-left">
           <div class="nama admin">
             <h2>Hai <?= "nama admin" ?></h2>
           </div>
@@ -12,7 +11,7 @@
             <input class="form-control mr-sm-2" id="search" type="search" placeholder="Search" aria-label="Search">
           </div>
         </div> -->
-        <!-- <div class="hero">
+      <!-- <div class="hero">
           <div class="px-4 py-5 my-1 text-center" id="hero">
             <i class="bi-slack" id="logo-rezerva" role="img" aria-label="Slack"></i>
             <img class="d-block mx-auto mb-4" src="" alt="logo rezerva" width="72" height="57">
@@ -26,20 +25,24 @@
             </div>
           </div>
         </div> -->
-        <div class="card card-chart">
-          <div class="card-header">
-            <div class="row ">
-              <div class="col">
-                <i> <h2 class="card-title">Pendapatan Bulanan</h2></i>
-              </div>
+      <div class="card card-chart">
+        <div class="card-header">
+          <div class="row ">
+            <div class="col">
+              <i>
+                <h2 class="card-title">Pendapatan Bulanan</h2>
+              </i>
             </div>
-            <div class="chart pb-0">
-              <div id="chart" class="card pb-0"></div>
-            </div>
+          </div>
+          <div class="chart pb-0">
+            <div id="chart" class="card pb-0"></div>
+          </div>
         </div>
       </div>
 
-      <div class="col-md-4">
+      
+    </div>
+    <div class="col-md-4">
         <div class="row justify-content-between align-items-center">
           <!-- <div class="col-md-12">
             <div class="card card-stats mb-3">
@@ -55,7 +58,7 @@
                   <i class="material-icons">Pendapatan Hari Ini</i>
                 </div>
                 <p class="card-category">Total</p>
-                <h3 class="card-title"><?=$data['pendapatan_hari_ini']?></h3>
+                <h3 class="card-title"><?= $data['pendapatan_hari_ini'] ?></h3>
               </div>
             </div>
           </div>
@@ -66,7 +69,7 @@
                   <i class="material-icons">Pendapatan Minggu Ini</i>
                 </div>
                 <p class="card-category">Total</p>
-                <h3 class="card-title"><?=$data['pendapatan_minggu_ini']?></h3>
+                <h3 class="card-title"><?= $data['pendapatan_minggu_ini'] ?></h3>
               </div>
             </div>
           </div>
@@ -77,7 +80,7 @@
                   <i class="material-icons">Pendapatan Bulan Ini</i>
                 </div>
                 <p class="card-category">Total</p>
-                <h3 class="card-title"><?=$data['pendapatan_bulan_ini']?></h3>
+                <h3 class="card-title"><?= $data['pendapatan_bulan_ini'] ?></h3>
               </div>
             </div>
           </div>
@@ -88,13 +91,12 @@
                   <i class="material-icons">Pendapatan Tahun Ini</i>
                 </div>
                 <p class="card-category">Total</p>
-                <h3 class="card-title"><?=$data['pendapatan_tahun_ini']?></h3>
+                <h3 class="card-title"><?= $data['pendapatan_tahun_ini'] ?></h3>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    
+
   </div>
