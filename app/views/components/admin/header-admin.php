0f4095b623682dfe41737807931db379b60e1f3a
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-18 23:26:04 +0700

    test

--- app/views/components/admin/header-admin.php
+++ app/views/components/admin/header-admin.php
@@ -4,17 +4,19 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="<?=BASEURL;?>/assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="<?=BASEURL;?>/assets/img/favicon.png">
-  <!-- Fonts and icons -->
+  <link rel="apple-touch-icon" sizes="76x76" href="<?= BASEURL; ?>/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="<?= BASEURL; ?>/assets/img/favicon.png">
+  <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
   <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
   <!-- Nucleo Icons -->
-  <link href="<?=BASEURL;?>/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="<?= BASEURL; ?>/assets/css/nucleo-icons.css" rel="stylesheet" />
   <!-- CSS Files -->
-  <link href="<?=BASEURL;?>/assets/css/black-dashboard.css?v=1.0.0" rel="stylesheet" />
+  <link href="<?= BASEURL; ?>/assets/css/black-dashboard.css?v=1.0.0" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="<?=BASEURL;?>/assets/demo/demo.css" rel="stylesheet" />
+  <link href="<?= BASEURL; ?>/assets/demo/demo.css" rel="stylesheet" />
+
+
   <!-- Bootstrap Icon -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
   <!-- Title -->
