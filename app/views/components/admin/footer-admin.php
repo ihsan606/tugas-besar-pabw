615c194c94e99d5365aac8bbb0a318ca981eaffa
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-18 22:33:51 +0700

    test

--- app/views/components/admin/footer-admin.php
+++ app/views/components/admin/footer-admin.php
@@ -76,7 +76,7 @@
       <script src="<?= BASEURL; ?>/assets/js/plugins/bootstrap-notify.js"></script>
       <!-- Control Center for Black Dashboard: parallax effects, scripts for the example pages etc -->
       <script src="<?= BASEURL; ?>/assets/js/black-dashboard.min.js?v=1.0.0"></script><!-- Black Dashboard DEMO methods, don't include it in your project! -->
-      <script src="<?= BASEURL; ?>/assets/demo/demo.js"></script>
+      <script src="assets/demo/demo.js"></script>
       <script>
         $(document).ready(function() {
           $().ready(function() {
