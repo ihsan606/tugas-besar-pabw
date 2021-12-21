80700655941202b9f9c549348d9b0b0621169e75
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-22 02:14:17 +0700

    update

--- app/views/pages/admin/dashboard.php
+++ app/views/pages/admin/dashboard.php
@@ -1,39 +1,16 @@
 <div class="container-fill">
   <!-- Laporan Pendapatan -->
   <div class="row">
-    <div class="col-md-8 pb-0 mb-0 d-flex">
-      <!-- <div class="card card-chart">
-        <div class="card-header">
-          <div class="row ">
-            <div class="col">
-              <i>
-                <h2 class="card-title">Pendapatan Bulanan</h2>
-              </i>
-            </div>
-          </div>
-          <div class="chart pb-0">
-            <div id="chart" class="card pb-0"></div>
-          </div>
-        </div>
-      </div> -->
+    <div class="col-md-9 pb-0 mb-0 d-flex">
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title text-center">Laporan Pendapatan Bulanan</h5>
+          <h3 class="text-center"><i class="material-icons">Pendapatan Bulanan Tahun 2021</i></h3>
           <canvas id="chartMonth"></canvas>
         </div>
       </div>
-      
     </div>
-    
-    <div class="col-md-4">
+    <div class="col-md-3">
       <div class="row justify-content-between align-items-center">
-        <!-- <div class="col-md-12">
-          <div class="card card-stats mb-3">
-            <div class="card-header card-header-success card-header-icon">
-              <i> <h2 class="card-title">Detail Pendapatan</h2></i>
-            </div>
-          </div>
-        </div> -->
         <div class="col-md-12">
           <div class="card card-stats">
             <div class="card-header card-header-success card-header-icon">
@@ -41,7 +18,7 @@
                 <i class="material-icons">Pendapatan Hari Ini</i>
               </div>
               <p class="card-category">Total</p>
-              <h3 class="card-title"><?= $data['pendapatan_hari_ini'] ?></h3>
+              <h3 class="card-title font-weight-bold" style="color: #2bffc6!important;"><?= $data['pendapatan_hari_ini'] ?></h3>
             </div>
           </div>
         </div>
@@ -52,7 +29,7 @@
                 <i class="material-icons">Pendapatan Minggu Ini</i>
               </div>
               <p class="card-category">Total</p>
-              <h3 class="card-title"><?= $data['pendapatan_minggu_ini'] ?></h3>
+              <h3 class="card-title font-weight-bold" style="color: #2bffc6!important;"><?= $data['pendapatan_minggu_ini'] ?></h3>
             </div>
           </div>
         </div>
@@ -63,7 +40,7 @@
                 <i class="material-icons">Pendapatan Bulan Ini</i>
               </div>
               <p class="card-category">Total</p>
-              <h3 class="card-title"><?= $data['pendapatan_bulan_ini'] ?></h3>
+              <h3 class="card-title font-weight-bold" style="color: #2bffc6!important;"><?= $data['pendapatan_bulan_ini'] ?></h3>
             </div>
           </div>
         </div>
@@ -74,7 +51,7 @@
                 <i class="material-icons">Pendapatan Tahun Ini</i>
               </div>
               <p class="card-category">Total</p>
-              <h3 class="card-title"><?= $data['pendapatan_tahun_ini'] ?></h3>
+              <h3 class="card-title font-weight-bold" style="color: #2bffc6!important;"><?= $data['pendapatan_tahun_ini'] ?></h3>
             </div>
           </div>
         </div>
@@ -129,41 +106,44 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.6.2/chart.js"></script>
   <?php 
     // memasukkan string ke dalam script chart
-    $dataBulanan=$data['pendapatan_perbulan'];
-    $insideData="";
-    for($i=0;$i<count($dataBulanan);$i++){
-      $insideData.=$dataBulanan[$i];
-      $insideData.=",";
-    }
+    $dataBulanan = $data['pendapatan_perbulan'];
+
+    // $data=[];
+
+    // $insideData = "";
+    // for ($i = 0; $i < count($dataBulanan); $i++) {
+    //   $insideData .= $dataBulanan[$i];
+    //   $insideData .= ",";
+    // }
 
     echo
     "<script>
-    const ct = document.getElementById('chartMonth').getContext('2d');
-    const chartMonth = new Chart(ct, {
-        type: 'bar',
-        data: {
-            labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
-            datasets: [{
-                label: '# of Votes',
-                data: [$insideData],
-                backgroundColor:'transparent',
-                borderColor:'#17a2b8',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            plugins: {
-                legend: {
-                    display: false
-                }
+        const ct = document.getElementById('chartMonth').getContext('2d');
+        const chartMonth = new Chart(ct, {
+            type: 'bar',
+            data: {
+                labels: ['JAN', 'FEB', 'MAR', 'APR', 'MEI', 'JUN', 'JUL', 'AGU', 'SEP', 'OKT', 'NOV', 'DES'],
+                datasets: [{
+                    label: 'Rp',
+                    data: [400000,300000,500000,600000,700000,500000,400000,800000,900000,600000,700000,$dataBulanan[11]],
+                    backgroundColor:'transparent',
+                    borderColor:'#11cdef',
+                    borderWidth: 2
+                }]
             },
-            scales: {
-                y: {
-                    beginAtZero: true
+            options: {
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                },
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
                 }
             }
-        }
-    });
-    </script>" 
-    ?>
+        });
+        </script>"
+        ?>
 
