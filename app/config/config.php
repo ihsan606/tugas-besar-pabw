53d7b142dca39f83a3ca21c4e878d99313b9c3be
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-21 13:47:12 +0700

    test

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-// define('BASEURL', 'http://localhost:8080/tugas-besar-pabw/public');
+define('BASEURL', 'http://localhost:8080/tugas-besar-pabw/public');
 // define('BASEURL', 'http://rezerva.test:4040/tugas-besar-pabw/public');
 // define('BASEURL', 'http://localhost:/tugas-besar-pabw/public');
 // define('BASEURL', 'https://rezerva.my.id/public');
