893b569d8bd55626fae15e3e5f44df74476aa4a9
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-22 02:38:28 +0700

    update

--- app/views/pages/admin/dashboard.php
+++ app/views/pages/admin/dashboard.php
@@ -118,33 +118,7 @@
   </div>
 
   <div class="row">
-    <div class="col-lg-4">
-      <div class="card card-chart">
-        <div class="card-header">
-          <h5 class="card-category">Total Shipments</h5>
-          <h3 class="card-title"><i class="tim-icons icon-bell-55 text-primary"></i> 763,215</h3>
-        </div>
-        <div class="card-body">
-          <div class="chart-area">
-            <canvas id="chartLinePurple"></canvas>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-4">
-      <div class="card card-chart">
-        <div class="card-header">
-          <h5 class="card-category">Daily Sales</h5>
-          <h3 class="card-title"><i class="tim-icons icon-delivery-fast text-info"></i> 3,500€</h3>
-        </div>
-        <div class="card-body">
-          <div class="chart-area">
-            <canvas id="CountryChart"></canvas>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-4">
+    <div class="col-lg-6">
       <div class="card card-chart">
         <div class="card-header">
           <h5 class="card-category">Completed Tasks</h5>
@@ -157,7 +131,19 @@
         </div>
       </div>
     </div>
-  </div>
+    <div class="col-lg-6">
+      <div class="card card-chart">
+        <div class="card-header">
+          <h5 class="card-category">Daily Sales</h5>
+          <h3 class="card-title"><i class="tim-icons icon-delivery-fast text-info"></i> 3,500€</h3>
+        </div>
+        <div class="card-body">
+          <div class="chart-area">
+            <canvas id="CountryChart"></canvas>
+          </div>
+        </div>
+      </div>
+    </div>
 </div>  
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.6.2/chart.js"></script>
