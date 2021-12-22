71c07aa77b17541a3b475ab2e81a7549ff664fb0
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-22 22:09:43 +0700

    alhamdulillah

--- app/views/components/admin/footer-admin.php
+++ app/views/components/admin/footer-admin.php
@@ -78,7 +78,6 @@
 
   <!-- Sweet Alert -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.33.1/sweetalert2.min.js"></script>
 
   <?php 
@@ -99,7 +98,7 @@
   // for ($i = 0; $i < count($dataBulanan); $i++) {
   //   $insideData .= $dataBulanan[$i];
   //   $insideData .= ",";
-  // 
+  
   echo"
     <script>
       const ct = document.getElementById('chartMonth').getContext('2d');
