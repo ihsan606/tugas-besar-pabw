c00f9de3e03162c358158af6cd744c27f7066bf9
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2021-12-22 23:56:24 +0700

    update

--- app/controllers/admin/Daftar_Pesanan_Controller.php
+++ app/controllers/admin/Daftar_Pesanan_Controller.php
@@ -1,9 +1,9 @@
 <?php 
 
 require '../vendor/autoload.php';
+
+use App\models\Customer;
 use App\models\Invoice;
-use App\models\Order;
-use Illuminate\Support\Carbon;
 
 class Daftar_Pesanan_Controller extends Controller{
   public function index()
@@ -18,6 +18,28 @@ class Daftar_Pesanan_Controller extends Controller{
       'diterima' => Invoice::where('status_pesanan', 'diterima')->count(),
     ];
     $this->view('daftar-pesanan', $data, 'admin');
+  }
+
+  public function show(){
+    $search = $_POST['search'];
+     if($search){
+      $customers_id = [];
+      foreach(Customer::where('name', 'like', '%'.$search."%")->get() as $customer){
+        array_push($customers_id, $customer->id); 
+      }
+
+      $data = [
+      'title' => 'Daftar Pesanan',
+      'invoices' => Invoice::with('orders.menu','orders.table','customer')->where('status_pesanan', 'dikonfirmasi')->whereIn('customer_id', $customers_id)->get(),
+      'dikonfirmasi' => Invoice::where('status_pesanan', 'dikonfirmasi')->count(),
+      'ditolak' => Invoice::where('status_pesanan', 'ditolak')->count(),
+      'diantar' => Invoice::where('status_pesanan', 'diantar')->count(),
+      'diterima' => Invoice::where('status_pesanan', 'diterima')->count(),
+    ];
+    $this->view('daftar-pesanan', $data, 'admin');
+    }else{
+      header('Location: ' . BASEURL . '/admin/daftar_pesanan');
+    }
   }
 
   public function antar_pesanan($id){
--- app/controllers/admin/Dashboard_Controller.php
+++ app/controllers/admin/Dashboard_Controller.php
@@ -2,6 +2,7 @@
 
 require '../vendor/autoload.php';
 use App\models\Invoice;
+use App\models\Customer;
 use App\models\Menu;
 use Illuminate\Support\Carbon;
 
@@ -24,9 +25,74 @@ class Dashboard_Controller extends Controller{
       'success' => Invoice::where('status_pembayaran', 'success')->count(),
       'expired' => Invoice::where('status_pembayaran', 'expired')->count(),
       'failed' => Invoice::where('status_pembayaran', 'failed')->count(),
-      'menus' => Menu::with('category')->get(),'invoices' => Invoice::with('orders.menu','orders.table','customer')->where('status_pesanan', 'dikonfirmasi')->get(),
-
+      'menus' => Menu::with('category')->get(),
+      'invoices' => Invoice::with('orders.menu','orders.table','customer')->where('status_pesanan', 'dikonfirmasi')->get(),
     ];
     $this->view('dashboard', $data, 'admin');
+  }
+
+  public function show_laporan_menu()
+  {
+    $search = $_POST['search'];
+
+    if($search){
+      $data_perbulan = [];
+      for($i = 1; $i < 13; $i++){
+        array_push($data_perbulan, Invoice::where('status_pembayaran', 'success')->whereMonth('created_at', $i)->sum('grand_total'));
+      }
+
+      $data = [
+        'title' => 'Dashboard',
+        'pendapatan_hari_ini' => money_format(Invoice::where('status_pembayaran', 'success')->where('created_at', '>=', Carbon::today())->sum('grand_total')),
+        'pendapatan_minggu_ini' => money_format(Invoice::where('status_pembayaran', 'success')->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->sum('grand_total')),
+        'pendapatan_bulan_ini' => money_format(Invoice::where('status_pembayaran', 'success')->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->sum('grand_total')),
+        'pendapatan_tahun_ini' => money_format(Invoice::where('status_pembayaran', 'success')->whereBetween('created_at', [Carbon::now()->startOfYear(), Carbon::now()->endOfYear()])->sum('grand_total')),
+        'pendapatan_perbulan' => $data_perbulan,
+        'pending' => Invoice::where('status_pembayaran', 'pending')->count(),
+        'success' => Invoice::where('status_pembayaran', 'success')->count(),
+        'expired' => Invoice::where('status_pembayaran', 'expired')->count(),
+        'failed' => Invoice::where('status_pembayaran', 'failed')->count(),
+        'menus' => Menu::where('title', 'like', '%'.$search.'%')->get(),
+        'invoices' => Invoice::with('orders.menu','orders.table','customer')->where('status_pesanan', 'dikonfirmasi')->get(),
+      ];
+      $this->view('dashboard', $data, 'admin');
+    }else{
+      header('Location: ' . BASEURL . '/admin/dashboard');
+    }
+  }
+
+  public function show_daftar_pesanan()
+  {
+    $search = $_POST['search'];
+
+    if($search){
+      $data_perbulan = [];
+      for($i = 1; $i < 13; $i++){
+        array_push($data_perbulan, Invoice::where('status_pembayaran', 'success')->whereMonth('created_at', $i)->sum('grand_total'));
+      }
+
+      $customers_id = [];
+      foreach(Customer::where('name', 'like', '%'.$search."%")->get() as $customer){
+        array_push($customers_id, $customer->id); 
+      }
+
+      $data = [
+        'title' => 'Dashboard',
+        'pendapatan_hari_ini' => money_format(Invoice::where('status_pembayaran', 'success')->where('created_at', '>=', Carbon::today())->sum('grand_total')),
+        'pendapatan_minggu_ini' => money_format(Invoice::where('status_pembayaran', 'success')->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->sum('grand_total')),
+        'pendapatan_bulan_ini' => money_format(Invoice::where('status_pembayaran', 'success')->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->sum('grand_total')),
+        'pendapatan_tahun_ini' => money_format(Invoice::where('status_pembayaran', 'success')->whereBetween('created_at', [Carbon::now()->startOfYear(), Carbon::now()->endOfYear()])->sum('grand_total')),
+        'pendapatan_perbulan' => $data_perbulan,
+        'pending' => Invoice::where('status_pembayaran', 'pending')->count(),
+        'success' => Invoice::where('status_pembayaran', 'success')->count(),
+        'expired' => Invoice::where('status_pembayaran', 'expired')->count(),
+        'failed' => Invoice::where('status_pembayaran', 'failed')->count(),
+        'menus' => Menu::where('title', 'like', '%'.$search.'%')->get(),
+        'invoices' => Invoice::with('orders.menu','orders.table','customer')->where('status_pesanan', 'dikonfirmasi')->whereIn('customer_id', $customers_id)->get(),
+      ];
+      $this->view('dashboard', $data, 'admin');
+    }else{
+      header('Location: ' . BASEURL . '/admin/dashboard');
+    }
   }
 }
